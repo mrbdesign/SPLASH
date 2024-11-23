89415b4e3c8d4be0b1d61cfc59925fe3f5ebdaee
mrbriandesign <128382133+mrbdesign@users.noreply.github.com>
2024-11-23 18:48:38 +0900

    Update mint subdomain link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             Mint NFT
           </a>
           <a
-            href="https://pixelworld-staking.vercel.app/"
+            href="https://mint.mrbd-pixel.world/"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
