8ce871a73d29e84365595fa7d25c51cc198ac2a4
mrbriandesign <128382133+mrbdesign@users.noreply.github.com>
2024-12-03 17:12:01 +0900

    fix: escape apostrophes for proper compilation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ function Header() {
       </p>
       <p className="text-zinc-300 text-base mt-2">
         500 boys + 500 girls, 32 x 32 px format, based on: <br />
-        'LITTLE WORLD PFPs' by Mr.Brian Design
+        LITTLE WORLD PFPs by Mr.Brian Design
       </p>
     </header>
   );
